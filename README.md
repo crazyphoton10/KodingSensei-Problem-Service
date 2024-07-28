dc30889f567a86599e785d67b383132ef8379945
Kumar Shantanu <thecrazyphoton@gmail.com>
2024-07-29 02:55:01 +0530

    Feat: Unimplemented Error for APIs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # KodingSensei Problem setting service
 
 # Problem Admin Service to prepare and store problems in DB. Add difficulty level, problem statement, likes, dislikes etc.
+
+# How Routing is working in the project?
+
+- /api/v1/problems/ping
+  - because the route starts with /api
+    - /api -> /v1 -> /problems -> /ping
+      - apiRouter -> v1Router -> problemRouter -> problemController -> service layer
--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -3,11 +3,21 @@ const problemController = require("../controllers");
 function pingProblemController(req, res) {
   return res.json({ msg: "Problem Controller is up" });
 }
-function addProblem(req, res) {}
-function getProblem(req, res) {}
-function getProblems(req, res) {}
-function deleteProblem(req, res) {}
-function updateProblem(req, res) {}
+function addProblem(req, res) {
+  return res.status(501).json({ msg: "Not Implemented" });
+}
+function getProblem(req, res) {
+  return res.status(501).json({ msg: "Not Implemented" });
+}
+function getProblems(req, res) {
+  return res.status(501).json({ msg: "Not Implemented" });
+}
+function deleteProblem(req, res) {
+  return res.status(501).json({ msg: "Not Implemented" });
+}
+function updateProblem(req, res) {
+  return res.status(501).json({ msg: "Not Implemented" });
+}
 
 module.exports = {
   pingProblemController,
