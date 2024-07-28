886d5d6774c2f22891b6b86bd2b76a746b886d16
Kumar Shantanu <thecrazyphoton@gmail.com>
2024-07-29 01:38:54 +0530

    Feat: Added routing structure for API for problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,19 @@
+const problemController = require("../controllers");
+
+function pingProblemController(req, res) {
+  return res.json({ msg: "Problem Controller is up" });
+}
+function addProblem(req, res) {}
+function getProblem(req, res) {}
+function getProblems(req, res) {}
+function deleteProblem(req, res) {}
+function updateProblem(req, res) {}
+
+module.exports = {
+  pingProblemController,
+  addProblem,
+  getProblem,
+  getProblems,
+  deleteProblem,
+  updateProblem,
+};
