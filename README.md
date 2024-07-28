cbcd4c3fe219acb7c4b4a0ff8e4e6420e9bedee6
Kumar Shantanu <thecrazyphoton@gmail.com>
2024-07-29 03:04:49 +0530

    Fix: Migrated from numeric Status codes to npm http status codes package for verbose status codes

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
   - because the route starts with /api
     - /api -> /v1 -> /problems -> /ping
       - apiRouter -> v1Router -> problemRouter -> problemController -> service layer
+
+res.status - returns same response object with status property set
+res.json - returns same response object which hasstatus set but json to be returned is also set
--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,22 +1,33 @@
 const problemController = require("../controllers");
+const { StatusCodes } = require("http-status-codes");
 
 function pingProblemController(req, res) {
   return res.json({ msg: "Problem Controller is up" });
 }
 function addProblem(req, res) {
-  return res.status(501).json({ msg: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ msg: "Not Implemented" });
 }
 function getProblem(req, res) {
-  return res.status(501).json({ msg: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ msg: "Not Implemented" });
 }
 function getProblems(req, res) {
-  return res.status(501).json({ msg: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ msg: "Not Implemented" });
 }
 function deleteProblem(req, res) {
-  return res.status(501).json({ msg: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ msg: "Not Implemented" });
 }
 function updateProblem(req, res) {
-  return res.status(501).json({ msg: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ msg: "Not Implemented" });
 }
 
 module.exports = {
